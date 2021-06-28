73e3012942486dcfc3a4a64136dbf0b86fabf0ac
Min Heo <min103ju@gmail.com>
2021-06-29 04:56:12 +0900

    test(car-game): 자동차 우승자 구하기 테스트 및 기능 구현

--- src/main/java/racing/CarGame.java
+++ src/main/java/racing/CarGame.java
@@ -1,20 +1,36 @@
 package racing;
 
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarGame {
     private final String carNames;
+    private final List<Car> cars;
+
+    private final String REGEX = ",";
 
     public CarGame(String carNames) {
         this.carNames = carNames;
+        this.cars = generateCars(0);
     }
 
-    public List<Car> initCars() {
-        return Arrays.stream(this.carNames.split(","))
+    public CarGame(String carNames, int position) {
+        this.carNames = carNames;
+        this.cars = generateCars(position);
+    }
+
+    public List<Car> generateCars(int position) {
+        return Arrays.stream(this.carNames.split(REGEX))
                 .map(name -> new CarName(name))
-                .map(carName -> new Car(carName))
+                .map(carName -> new Car(carName, new Position(position)))
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> getWinners(int winnerPosition) {
+        return this.cars.stream()
+                .filter(car -> car.isWinner(winnerPosition))
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/racing/CarGameTest.java
+++ src/test/java/racing/CarGameTest.java
@@ -9,6 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarGameTest {
 
+    @DisplayName("우승자 구하기 기능 테스트")
+    @Test
+    void winnersTest() {
+        // given
+        CarGame carGame = new CarGame("car1,car2,car3", 3);
+
+        // when
+        List<Car> winners = carGame.getWinners(3);
+
+        // then
+        assertThat(winners.size()).isEqualTo(3);
+    }
+
     @DisplayName("자동차 이름 쉼표 기준으로 분리 테스트")
     @Test
     void splitCar() {
@@ -16,7 +29,7 @@ public class CarGameTest {
         CarGame carGame = new CarGame("car1,car2,car3");
 
         // when
-        List<Car> cars = carGame.initCars();
+        List<Car> cars = carGame.generateCars(0);
 
         // then
         assertThat(cars.size()).isEqualTo(3);
